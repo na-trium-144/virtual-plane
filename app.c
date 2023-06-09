54114ffafecf7f87f502c7e40db29c129d96315f
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-06-09 16:13:00 +0900

    2dと3dを重ねる

--- app.c
+++ app.c
@@ -55,12 +55,18 @@ void init(void)
 }
 
 double z = 0;
-
+double aspect = 1;
 void display(void)
 {
-  
+
    glClear (GL_COLOR_BUFFER_BIT);
-   
+
+  glMatrixMode (GL_PROJECTION);
+   glLoadIdentity ();
+   // glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
+   gluPerspective(60, aspect, 1.5, 20);
+   glMatrixMode (GL_MODELVIEW);
+  
    // 以下はsample-cube.cのコピペ
    
    glColor3f (0.5, 0.3, 0.0);
@@ -94,17 +100,31 @@ void display(void)
    //glutWireCone(0.5, 2, 30, 30);
    
    //glFlush ();
+
+
+      glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(0, 1, 0, 1);
+   glMatrixMode(GL_MODELVIEW);
+   glLoadIdentity();
+   glColor3f(1, 1, 1);
+   glBegin(GL_POLYGON);
+   glVertex3f(0, 0, 0);
+   glVertex3f(0, 0.3, 0);
+   glVertex3f(0.3, 0.3, 0);
+   glVertex3f(0.3, 0, 0);
+   glEnd();
+
+
+   glFlush();
+   
    glutSwapBuffers();
 }
 
 void reshape (int w, int h)
 {
    glViewport (0, 0, (GLsizei) w, (GLsizei) h);
-   glMatrixMode (GL_PROJECTION);
-   glLoadIdentity ();
-   // glFrustum (-1.0, 1.0, -1.0, 1.0, 1.5, 20.0);
-   gluPerspective(60, (double)w / h, 1.5, 20);
-   glMatrixMode (GL_MODELVIEW);
+   aspect = (double)w / h;
 }
 
 /* ARGSUSED1 */
