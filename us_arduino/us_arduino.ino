9bd3fdf76600a8d2669d11a30ee6efdc4c855676
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-06-09 15:46:40 +0900

    copy_of_3のコピー

---
+++ us_arduino/us_arduino.ino
@@ -0,0 +1,23 @@
+#define TRIG_PIN 2 // set digital pin ID
+#define ECHO_PIN 3
+const float time_gain = 1.0/58; // gain for duration [us] to distance [cm]
+float read_sensor(){
+// trigger
+digitalWrite(TRIG_PIN, HIGH);
+delayMicroseconds(10); // 10 usec
+digitalWrite(TRIG_PIN, LOW);
+// measure the duration when echo is high
+return time_gain*pulseIn(ECHO_PIN,HIGH);
+}
+void setup() {
+// ultrasonic sensor
+pinMode(TRIG_PIN, OUTPUT);
+pinMode(ECHO_PIN, INPUT);
+Serial.begin(9600);
+}
+float distance;
+void loop() {
+distance = read_sensor();
+Serial.println(distance);
+delay(100);
+}
